ea9c08324be96e51d1495b262d8a4a9406243c75
Chika Nna <chika.ekene@gmail.com>
2020-12-08 01:06:41 -0500

    alot of changes

---
+++ js/script.js
@@ -0,0 +1,23 @@
+CountersFunction = () => {
+  const counters = document.querySelectorAll(".counter");
+  const speed = 10;
+
+  counters.forEach((counter) => {
+    const updateCount = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText;
+
+      const inc = target / speed;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + inc);
+        setTimeout(updateCount, 100);
+      } else {
+        count.innerText = target;
+      }
+    };
+    updateCount();
+  });
+};
+
+CountersFunction();
